d423c5f7b7e65464fe6c46e1c7c3da95c2cf783d
franfiori29 <franfiori@gmail.com>
2020-11-03 16:46:34 -0300

    ABM subida2

--- Php/ejer26ABM/script.js
+++ Php/ejer26ABM/script.js
@@ -1,155 +1,11 @@
 
-
 $("#load").click(cargaTabla);
 $("#altaEmpleado").click(abrirAlta);
 $("#botonCerrarModal").click(cerrarAlta);
 $("#formAlta").change(validAlta);
 $("#altaValidar").click(Alta);
 $("#botonCerrarModif").click(cerrarModif);
-$("#modiValidar").click(ejecutarUpdate);
-
-
-function borrar(idEmpleado) {
-    if (confirm("Esta seguro que quiere eliminar el empelado?")) {
-        var objAjax = $.ajax({
-            type: "get",
-            url: "./delete.php",
-            data: {
-                idEmpleado: idEmpleado
-            },
-            success: function () {
-                cargaTabla();
-                cargaTabla();
-            }
-        })
-    } else {
-        alert("Su acción fue cancelada");
-    }
-}
-
-function ejecutarBorrar() {
-    var i = this.document.activeElement.getAttribute("class");
-    borrar(i);
-}
-
-function validAlta() {
-    if ($("#altaID").isValid() &&
-        $("#altaApellido").isValid() &&
-        $("#altaTelefono").isValid() &&
-        $("#altaArea").isValid() &&
-        $("#altaNombre").isValid() &&
-        $("#altaFechaAlta").isValid()) {
-
-        $("#altaValidar").attr("disabled", false);
-    } else {
-        $("#altaValidar").attr("disabled", true);
-    }
-};
-
-
-function Alta() {
-    if (confirm("Esta Seguro que quiere dar de alta?")) {
-        $objAjax = $.ajax({
-            type: "post",
-            url: "./alta.php",
-            data: {
-                idEmpleado: $("#altaID").val(),
-                telefono: $("#altaTelefono").val(),
-                apellido: $("#altaApellido").val(),
-                area: $("#altaArea").val(),
-                nombre: $("#altaNombre").val(),
-                fechaAlta: $("#altaFechaAlta").val()
-            },
-            success: function () {
-                cargaTabla();
-                cargaTabla();
-                cerrarAlta()
-
-            }
-        })
-    } else {
-        alert("Se ha cancelado el alta");
-    }
-};
-
-function actualizarEmpleado(idEmpleado) {
-    if (confirm("Estas seguro de cambiar estos datos?")) {
-        $objAjax = $.ajax({
-            type: "POST",
-            url: "./update.php",
-            data: {
-                idEmpleado: idEmpleado,
-                apellido: $("#modiApellido").val(),
-                telefono: $("#modiTelefono").val(),
-                area: $("#modiArea").val(),
-                nombre: $("#modiNombre").val(),
-                fechaAlta: $("#modiFechaAlta").val()
-            },
-            success: function () {
-                cargaTabla();
-                cerrarModif();
-            }
-        })
-    } else {
-        alert("Has cancelado la operación");
-    }
-};
-
-function ejecutarUpdate() {
-    actualizarEmpleado(savedValue);
-};
-
-
-$(document).ready(function () {
-    $("#formAlta").css("display", "none");
-    $("#formModi").css("display", "none");
-});
-
-function abrirAlta() {
-    $("#formAlta").css("display", "block");
-    $("#doc").addClass("modalDesactivado");
-};
-
-function cerrarAlta() {
-    $("#formAlta").css("display", "none");
-    $("#doc").removeClass("modalDesactivado");
-};
-
-function cerrarModif() {
-    $("#formModi").css("display", "none");
-    $("#doc").removeClass("modalDesactivado");
-};
-
-
-var savedValue;
-
-function abrirModalModi() {
-    $("#formModi").css("display", "block");
-    var i = this.document.activeElement.getAttribute("class");
-    savedValue = i;
-    completarFichaModi(i);
-    $("#doc").addClass("modalDesactivado");
-};
-
-function completarFichaModi(idEmpleado) {
-    $("#formModi").val(idEmpleado);
-    var objAjax = $.ajax({
-        type: "get",
-        url: "./modi.php",
-        data: { idEmpleado: idEmpleado },
-        success: function (respuesta, estado) {
-            objetoEmpl = JSON.parse(respuesta);
-
-            $("#modiID").val(objetoEmpl.idEmpleado);
-            $("#modiApellido").val(objetoEmpl.apellido);
-            $("#modiTelefono").val(objetoEmpl.telefono);
-            $("#modiArea").val(objetoEmpl.area);
-            $("#modiNombre").val(objetoEmpl.nombre);
-            $("#modiFechaAlta").val(objetoEmpl.fechaAlta);
-        }
-    });
-};
-
+$("#modiValidar").click(hacerUpdate);
 
 function cargaTabla() {
     $("#contenidoTabla").empty();
@@ -206,7 +62,7 @@ function cargaTabla() {
                 tdMod.innerHTML = `<button id='modi' class='${value.idEmpleado}'onclick='abrirModalModi()'>MODIFICAR</button>`;
 
                 var tdBaja = document.createElement("td");
-                tdBaja.innerHTML = `<button id='baja' class='${value.idEmpleado}'onclick='ejecutarBorrar()'>BORRAR</button>`;
+                tdBaja.innerHTML = `<button id='baja' class='${value.idEmpleado}'onclick='hacerBorrado()'>BORRAR</button>`;
 
 
 
@@ -220,10 +76,148 @@ function cargaTabla() {
     });
 };
 
+function validAlta() {
+    if ($("#altaID").isValid() &&
+        $("#altaApellido").isValid() &&
+        $("#altaTelefono").isValid() &&
+        $("#altaArea").isValid() &&
+        $("#altaNombre").isValid() &&
+        $("#altaFechaAlta").isValid()) {
+
+        $("#altaValidar").attr("disabled", false);
+    } else {
+        $("#altaValidar").attr("disabled", true);
+    }
+};
+
+function Alta() {
+    if (confirm("Esta Seguro que quiere dar de alta?")) {
+        $objAjax = $.ajax({
+            type: "post",
+            url: "./alta.php",
+            data: {
+                idEmpleado: $("#altaID").val(),
+                telefono: $("#altaTelefono").val(),
+                apellido: $("#altaApellido").val(),
+                area: $("#altaArea").val(),
+                nombre: $("#altaNombre").val(),
+                fechaAlta: $("#altaFechaAlta").val()
+            },
+            success: function () {
+                cargaTabla();
+                cargaTabla();
+                cerrarAlta()
+
+            }
+        })
+    } else {
+        alert("Se ha cancelado el alta");
+    }
+};
+
+function borrar(idEmpleado) {
+    if (confirm("Esta seguro que quiere eliminar el empelado?")) {
+        var objAjax = $.ajax({
+            type: "get",
+            url: "./delete.php",
+            data: {
+                idEmpleado: idEmpleado
+            },
+            success: function () {
+                cargaTabla();
+                cargaTabla();
+            }
+        })
+    } else {
+        alert("Su acción fue cancelada");
+    }
+}
+
+function hacerBorrado() {
+    var i = this.document.activeElement.getAttribute("class");
+    borrar(i);
+}
+
+function actualizarEmpleado(idEmpleado) {
+    if (confirm("Estas seguro de cambiar estos datos?")) {
+        $objAjax = $.ajax({
+            type: "POST",
+            url: "./update.php",
+            data: {
+                idEmpleado: idEmpleado,
+                apellido: $("#modiApellido").val(),
+                telefono: $("#modiTelefono").val(),
+                area: $("#modiArea").val(),
+                nombre: $("#modiNombre").val(),
+                fechaAlta: $("#modiFechaAlta").val()
+            },
+            success: function () {
+                cargaTabla();
+                cerrarModif();
+            }
+        })
+    } else {
+        alert("Has cancelado la operación");
+    }
+};
+
+function hacerUpdate() {
+    actualizarEmpleado(valorACambiar);
+};
+
+
+$(document).ready(function () {
+    $("#formAlta").css("display", "none");
+    $("#formModi").css("display", "none");
+});
+
+function abrirAlta() {
+    $("#formAlta").css("display", "block");
+    $("#doc").addClass("modalDesactivado");
+};
+
+function cerrarAlta() {
+    $("#formAlta").css("display", "none");
+    $("#doc").removeClass("modalDesactivado");
+};
+
+function cerrarModif() {
+    $("#formModi").css("display", "none");
+    $("#doc").removeClass("modalDesactivado");
+};
+
+var valorACambiar;
+
+function abrirModalModi() {
+    $("#formModi").css("display", "block");
+    var i = this.document.activeElement.getAttribute("class");
+    valorACambiar = i;
+    completarFichaModi(i);
+    $("#doc").addClass("modalDesactivado");
+};
+
+function completarFichaModi(idEmpleado) {
+    $("#formModi").val(idEmpleado);
+    var objAjax = $.ajax({
+        type: "get",
+        url: "./modi.php",
+        data: { idEmpleado: idEmpleado },
+        success: function (respuesta, estado) {
+            objetoEmpl = JSON.parse(respuesta);
+
+            $("#modiID").val(objetoEmpl.idEmpleado);
+            $("#modiApellido").val(objetoEmpl.apellido);
+            $("#modiTelefono").val(objetoEmpl.telefono);
+            $("#modiArea").val(objetoEmpl.area);
+            $("#modiNombre").val(objetoEmpl.nombre);
+            $("#modiFechaAlta").val(objetoEmpl.fechaAlta);
+        }
+    });
+};
+
 $("#delete").click(function () {
     $("#contenidoTabla").empty();
 });
-
 
 $(document).ready(function () {
     $("#thIdEmpleado").click(function () {
@@ -243,28 +237,24 @@ $(document).ready(function () {
         cargaTabla();
     });
 });
-
 $(document).ready(function () {
     $("#thEmplArea").click(function () {
         $("#orden").val("area");
         cargaTabla();
     });
 });
-
 $(document).ready(function () {
     $("#thEmplNombre").click(function () {
         $("#orden").val("nombre");
         cargaTabla();
     });
 });
-
 $(document).ready(function () {
     $("#thEmplFA").click(function () {
         $("#orden").val("fechaAlta");
         cargaTabla();
     });
 });
-
 
 $.fn.isValid = function () {
     return this[0].checkValidity()
